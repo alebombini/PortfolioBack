94f7172bffe7018ef3c3bf1188345cf47c3eea42
alebombini <alejandrabombi@hotmail.com>
2022-11-30 08:57:42 -0300

    creacion proyecto backend

---
+++ AleBombini/src/main/java/com/portfolio/AleBombini/AleBombiniApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.AleBombini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleBombiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AleBombiniApplication.class, args);
+	}
+
+}
---
+++ AleBombini/src/test/java/com/portfolio/AleBombini/AleBombiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.AleBombini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleBombiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
